bcff2550394d1b08c98dc1cf680914aca447fff0
Lord-K <kusilordkwame105@gmail.com>
2022-09-27 18:14:49 +0000

    updated email v2

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
+  email: {type: String, required: true},
   username: { type: String, required: true },
   password: { type: String, required: true },
-  studentId: String,
   serviceType: { type: String, required: true },
   serviceTags: [String],
   profilePic: String,
