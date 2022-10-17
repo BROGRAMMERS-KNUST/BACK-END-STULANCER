414c35ab34c088c6e4150ae2d2f6903c9dceace8
Wassem Darkwa <waseemdarkwa@gmail.com>
2022-10-17 12:52:35 +0100

    Minor update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 const PORT = process.env.PORT;
 
 mongoose
-  .connect(process.env.MONGO_URI_LOCAL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
